69a1db6737f243fd63e8beb5a8948cd86a84774d
Kajetan Pynka <siema1234@gazeta.pl>
2024-12-22 16:11:17 +0100

    Updated basePath and readme

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   // If you're not deploying to the root domain, add this:
-  basePath: '/your-repo-name',
+  basePath: '/flashcards-maker',
   images: {
     unoptimized: true,
   }
